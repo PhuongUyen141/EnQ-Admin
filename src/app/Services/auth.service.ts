00af1e798134aa9f4777e42daf7f94b00a11307c
NguyenVanMinhTuan <tuan01102000@gmail.com>
2020-09-23 15:34:45 +0700

    login tren service (dang test) voi form tren login page (chua css)

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { API } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   constructor(private httpClient: HttpClient) { }
+
+  login(username: string, password: string): Observable<unknown> {
+    return this.httpClient.post(API + 'users/admin/login', {
+      username, password
+    });
+  }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./Module/login-page/login-page.module').then(m => m.LoginPageModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
